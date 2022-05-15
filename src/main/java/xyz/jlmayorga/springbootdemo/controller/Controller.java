ea15a058c5b2c51ba48eb950ec778986eaed6168
Jose Mayorga <jmayorga@redhat.com>
2022-05-15 01:22:39 -0500

    Test updates

--- src/main/java/xyz/jlmayorga/springbootdemo/controller/Controller.java
+++ src/main/java/xyz/jlmayorga/springbootdemo/controller/Controller.java
@@ -1,14 +1,26 @@
 package xyz.jlmayorga.springbootdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("hello")
 public class Controller {
+    @Value("${CHART_VERSION:0}")
+    String chartVersion;
+    @Value("${CONTAINER_IMAGE:0}")
+    String containerImage;
+
     @GetMapping(value = "/", produces = "application/json")
-    public String sayHello() {
-        return "Hello, Codefresh!";
+    public Map<String, String> sayHello() {
+        Map<String, String> returnMap = new HashMap<>();
+        returnMap.put("message", "Hello, Codefresh!");
+        returnMap.put("chartVersion", chartVersion);
+        returnMap.put("containerImage", containerImage);
     }
 }
